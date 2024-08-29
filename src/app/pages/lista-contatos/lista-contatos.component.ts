186fae3e001ffdb7f4743cb01df457b7207db14a
F4844758 Joao Emanoel Rodrigues Mota <joao_mota.jm@bb.com.br>
2024-08-29 13:29:45 -0300

    Ok

--- src/app/pages/lista-contatos/lista-contatos.component.ts
+++ src/app/pages/lista-contatos/lista-contatos.component.ts
@@ -8,7 +8,7 @@ import { ContatoService } from 'src/app/services/contato.service';
   styleUrls: ['./lista-contatos.component.css']
 })
 export class ListaContatosComponent {
-  // alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+  // alfabeto = 'abcdefghijklmnopqrstuvwxyz';
   alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   contatos: Contato[] = [];
 
